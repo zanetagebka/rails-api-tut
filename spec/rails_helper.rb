cf14d24888d75a3aa59bf5200f542361f90d5303
Zaneta Gebka <43746213+zanetagebka@users.noreply.github.com>
2020-02-25 23:33:22 +0100

    Controllers - concerns for exception handling - controller specs - controllers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,11 +41,14 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
   config.include FactoryBot::Syntax::Methods
+  config.include RequestSpecHelper, type: :request
 
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
